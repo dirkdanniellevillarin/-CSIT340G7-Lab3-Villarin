506cf56413e2c5a0cf6443269886241dca16351c
fruittail <villarindirkdannielle@gmail.com>
2026-09-21 18:00:05 +0800

    1.4: course information step 4

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,22 @@ function Part(props) {
 function Content(props) {
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 }
 
 function Total(props) {
-  return <p>Total Units: {props.total}</p>;
+  return (
+    <p>
+      Total Units:{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
 }
 
 function Footer(props) {
@@ -38,24 +45,26 @@ function Footer(props) {
 function App() {
   const course = "Application Development";
 
-  const part1 = {
-    name: "IT365 - Data Analytics",
-    exercises: 3,
-  };
-  const part2 = {
-    name: "CSIT327 - Information Management 2",
-    exercises: 3,
-  };
-  const part3 = {
-    name: "CSIT340 - Industry Elective",
-    exercises: 3,
-  };
+  const parts = [
+    {
+      name: "IT365 - Data Analytics",
+      exercises: 3,
+    },
+    {
+      name: "CSIT327 - Information Management 2",
+      exercises: 3,
+    },
+    {
+      name: "CSIT340 - Industry Elective",
+      exercises: 3,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
       <Footer
         fullName="Dirk Dannielle E. Villarin"
         courseCode="CSIT340"
